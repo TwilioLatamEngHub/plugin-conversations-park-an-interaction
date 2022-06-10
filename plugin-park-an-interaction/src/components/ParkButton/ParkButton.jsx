d373642827c518b253afbb6ca4a44766ce9cd4b3
Gus Varallo <gustavo@varallo.me>
2022-06-10 11:01:55 -0300

    feat: update ParkButton component

--- plugin-park-an-interaction/src/components/ParkButton/ParkButton.jsx
+++ plugin-park-an-interaction/src/components/ParkButton/ParkButton.jsx
@@ -1,34 +1,74 @@
-import React from 'react'
-import { Actions, withTheme } from '@twilio/flex-ui'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Actions } from '@twilio/flex-ui'
 
-import { StyledButton } from './ParkButton.styles'
+import { Button } from '@twilio-paste/core/button'
+import { Theme } from '@twilio-paste/core/theme'
 
-export class ParkButton extends React.PureComponent {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isDisabled: false,
-      cursor: 'pointer'
-    }
-  }
+const ButtonWrapper = styled.div`
+  margin: 1rem;
+  font-size: 1rem;
+  color: #fff;
+`
 
-  render() {
-    return (
-      <StyledButton
-        color={this.props.theme.tokens.textColors}
-        background={this.props.theme.tokens.backgroundColors}
-        disabled={this.state.isDisabled}
-        style={{ cursor: this.state.cursor }}
-        onClick={() => {
-          this.setState({ isDisabled: true })
-          this.setState({ cursor: 'not-allowed' })
-          Actions.invokeAction('ParkInteraction', { task: this.props.task })
-        }}
-      >
-        Pause
-      </StyledButton>
-    )
-  }
+const ParkButton = props => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [cursor, setCursor] = useState('pointer')
+
+  return (
+    <Theme.Provider theme='default'>
+      <ButtonWrapper>
+        <Button
+          variant='reset'
+          size='reset'
+          loading={isLoading}
+          style={{ cursor: cursor }}
+          onClick={() => {
+            setIsLoading(true)
+            setCursor('not-allowed')
+            Actions.invokeAction('ParkInteraction', { task: props.task })
+          }}
+        >
+          Pause
+        </Button>
+      </ButtonWrapper>
+    </Theme.Provider>
+  )
 }
 
-export default withTheme(ParkButton)
+export default ParkButton
+
+// import React from 'react'
+// import { Actions, withTheme } from '@twilio/flex-ui'
+
+// import { StyledButton } from './ParkButton.styles'
+
+// export class ParkButton extends React.PureComponent {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       isDisabled: false,
+//       cursor: 'pointer'
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <StyledButton
+//         color={this.props.theme.tokens.textColors}
+//         background={this.props.theme.tokens.backgroundColors}
+//         disabled={this.state.isDisabled}
+//         style={{ cursor: this.state.cursor }}
+//         onClick={() => {
+//           this.setState({ isDisabled: true })
+//           this.setState({ cursor: 'not-allowed' })
+//           Actions.invokeAction('ParkInteraction', { task: this.props.task })
+//         }}
+//       >
+//         Pause
+//       </StyledButton>
+//     )
+//   }
+// }
+
+// export default withTheme(ParkButton)
