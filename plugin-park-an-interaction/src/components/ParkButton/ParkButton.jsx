bfa0c4664421938bd39b54b9ef6cff1263accef9
Gus Varallo <gustavo@varallo.me>
2022-06-10 11:03:04 -0300

    refactor: remove unecessary comments

--- plugin-park-an-interaction/src/components/ParkButton/ParkButton.jsx
+++ plugin-park-an-interaction/src/components/ParkButton/ParkButton.jsx
@@ -37,38 +37,3 @@ const ParkButton = props => {
 }
 
 export default ParkButton
-
-// import React from 'react'
-// import { Actions, withTheme } from '@twilio/flex-ui'
-
-// import { StyledButton } from './ParkButton.styles'
-
-// export class ParkButton extends React.PureComponent {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       isDisabled: false,
-//       cursor: 'pointer'
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <StyledButton
-//         color={this.props.theme.tokens.textColors}
-//         background={this.props.theme.tokens.backgroundColors}
-//         disabled={this.state.isDisabled}
-//         style={{ cursor: this.state.cursor }}
-//         onClick={() => {
-//           this.setState({ isDisabled: true })
-//           this.setState({ cursor: 'not-allowed' })
-//           Actions.invokeAction('ParkInteraction', { task: this.props.task })
-//         }}
-//       >
-//         Pause
-//       </StyledButton>
-//     )
-//   }
-// }
-
-// export default withTheme(ParkButton)
