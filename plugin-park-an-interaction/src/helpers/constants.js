006a75f44d1de2c3151f8c9115c111ef0a8e9f42
Gus Varallo <gustavo@varallo.me>
2022-06-10 13:14:49 -0300

    refactor: update env example

--- plugin-park-an-interaction/src/helpers/constants.js
+++ plugin-park-an-interaction/src/helpers/constants.js
@@ -1,4 +1,2 @@
-export const URL_PARK_AN_INTERACTION =
-  'http://localhost:3000/park-an-interaction'
-export const URL_CLOSE_AN_INTERACTION =
-  'http://localhost:3000/close-an-interaction'
+export const URL_PARK_AN_INTERACTION = ''
+export const URL_CLOSE_AN_INTERACTION = ''
