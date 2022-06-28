028c4a982d1fe2309a621307ad8187d8c72059db
Gus Varallo <gustavo@varallo.me>
2022-06-28 10:06:23 -0300

    refactor: rename folder

---
+++ plugin-park-an-interaction/src/utils/constants.js
@@ -0,0 +1,4 @@
+export const URL_PARK_AN_INTERACTION =
+  'https://serverless-park-an-interaction-4235-dev.twil.io/park-an-interaction'
+export const URL_CLOSE_AN_INTERACTION =
+  'https://serverless-park-an-interaction-4235-dev.twil.io/close-an-interaction'
